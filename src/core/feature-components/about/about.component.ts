cb8b7054d4f436cdadff10c541d7e37a0af8c919
drsamarendra <drsamarendranathkhara@gmail.com>
2025-07-14 18:35:43 +0530

    modified

--- src/core/feature-components/about/about.component.ts
+++ src/core/feature-components/about/about.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BlogService } from '../../shared-service/blog.service';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
   public blogList: any[] = [];
-  constructor(private blogService: BlogService) { }
+  private blogServiceSubscription: any;
+
+  constructor(private blogService: BlogService,
+    private router: Router) { }
 
   ngOnInit(): void {
-    this.blogService.handleBlogList().subscribe((data: any) => {
+    this.blogServiceSubscription = this.blogService.handleBlogList().subscribe(data => {
       this.blogList = data;
-    }, (error: any) => {
-      console.error('Error fetching blog list:', error);
     });
   }
 
+  goToBlogDetails(blog: any): void {
+    const navigationExtras: NavigationExtras = {
+      state: {
+        data: blog
+      }
+    };
+    this.router.navigateByUrl('/blog-details', navigationExtras);
+  }
+
+  ngOnDestroy(): void {
+    this.blogServiceSubscription?.unsubscribe();
+  }
 }
