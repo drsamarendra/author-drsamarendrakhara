3920a70bc5c2827429a0b89e488d456769fbc3c1
drsamarendra <drsamarendranathkhara@gmail.com>
2025-07-16 23:30:23 +0530

    modified

--- src/core/core-routing.module.ts
+++ src/core/core-routing.module.ts
@@ -7,11 +7,7 @@ import { ProductDetailsComponent } from './feature-components/product-details/pr
 import { BlogDetailsComponent } from './feature-components/blog-details/blog-details.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  
   {
     path: 'home',
     component: HomeComponent,
@@ -32,6 +28,11 @@ const routes: Routes = [
   {
     path: 'blog-details',
     component: BlogDetailsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
     path: '**',
--- src/core/template/template.component.ts
+++ src/core/template/template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template.component.css']
 })
 export class TemplateComponent implements OnInit {
-
+  public myDynamicStyles: any = { 'margin-top': '20px' };
   constructor() { }
 
   ngOnInit(): void {
+    this.fixMarginTop();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.fixMarginTop();
+  }
+
+  private fixMarginTop() {
+    if (window.innerWidth >= 187 && window.innerWidth <= 357) {
+      this.myDynamicStyles = { 'margin-top': '190px' };
+    } else if (window.innerWidth >= 358 && window.innerWidth <= 339) {
+      this.myDynamicStyles = { 'margin-top': '150px' };
+    } else if (window.innerWidth >= 340 && window.innerWidth <= 767) {
+      this.myDynamicStyles = { 'margin-top': '130px' };
+    } else if (window.innerWidth >= 768) {
+      this.myDynamicStyles = { 'margin-top': '100px' };
+    }
   }
 
 }
