48da978011ccb701593da0033b5f6eaa90612fff
drsamarendra <drsamarendranathkhara@gmail.com>
2025-07-19 15:54:23 +0530

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log("hello app")
+    localStorage.removeItem('lang');
+  }
+
+  ngOnDestroy(): void {
+    
+  }
+
 }
--- src/core/feature-components/about/about.component.ts
+++ src/core/feature-components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { GetApiDataService } from 'src/core/shared-service/get-api-data.service';
 
 @Component({
@@ -8,23 +8,50 @@ import { GetApiDataService } from 'src/core/shared-service/get-api-data.service'
 })
 export class AboutComponent implements OnInit, OnDestroy {
   public componentData: any = {};
-  public languageData: { en?: any; bn?: any } = {};
+  public languageData: any = {};
   public isEnglish = true;
+  public dynamicStyles: any = {};
 
   constructor(private apiData: GetApiDataService) { }
 
   ngOnInit(): void {
     this.apiData.getApiData('json/language_data.json').subscribe(response => {
       this.languageData = response.data;
-      this.componentData = this.isEnglish ? this.languageData['en'] : this.languageData['bn'];
+      localStorage.getItem("lang");
+      const lang = localStorage.getItem("lang") || 'en';
+      this.isEnglish = (lang == 'en')
+      this.componentData = this.languageData[lang];
     });
   }
 
   toggleLanguage() {
+    console.log("hello")
     this.isEnglish = !this.isEnglish;
-    this.componentData = this.isEnglish ? this.languageData['en'] : this.languageData['bn'];
+    const lang = this.isEnglish ? "en" : 'bn';
+    localStorage.setItem("lang", lang);
+    this.componentData = this.languageData[lang];
+
   }
 
-  ngOnDestroy(): void { }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.fixMarginTop();
+  }
+
+  private fixMarginTop() {
+    if (window.innerWidth >= 187 && window.innerWidth <= 357) {
+      this.dynamicStyles = { 'margin-top': '190px' };
+    } else if (window.innerWidth >= 358 && window.innerWidth <= 339) {
+      this.dynamicStyles = { 'margin-top': '150px' };
+    } else if (window.innerWidth >= 340 && window.innerWidth <= 767) {
+      this.dynamicStyles = { 'margin-top': '130px' };
+    } else if (window.innerWidth >= 768) {
+      this.dynamicStyles = { 'margin-top': '100px' };
+    }
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.fixMarginTop.bind(this));
+  }
 
 }
